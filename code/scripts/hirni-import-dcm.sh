13fb80d8caef99aedc38c188d62d493a0998ee87
Kadelka, Tobias <t.kadelka@fz-juelich.de>
2019-09-05 09:11:36 +0200

    the path was wrong, I changed it.

--- code/scripts/hirni-import-dcm.sh
+++ code/scripts/hirni-import-dcm.sh
@@ -1,6 +1,4 @@
 for d in /data/BnB_USER/Kadelka/SOURCEDATA/* ; do
-        for b in $d/* ; do
-                echo $b
-                datalad hirni-import-dcm $b
-        done
+                datalad hirni-import-dcm $d
 done
+
