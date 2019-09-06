5b2c9a0fd4a281d8e198a37e44fe5e9c7c384a1b
Kadelka, Tobias <t.kadelka@fz-juelich.de>
2019-09-06 15:14:09 +0200

    DTI is missing, probably because of missing task/modality in studyspec

--- code/procedures/fix-fieldmaps-names.sh
+++ code/procedures/fix-fieldmaps-names.sh
@@ -2,7 +2,7 @@
 dataset=${1}
 subject=${2}
 
-cd /data/BnB_USER/Kadelka/DTA/
+cd dataset
 
 for old_json in sub-DTA*/*/fmap/*fieldmap*.json ; do
 	if [ ! -z $(cat ${old_json} | jq .ImageType  | grep -v "ND" | grep -v "ORIGINAL"| grep -v "PRIMARY" | grep "P" ) ]; then
