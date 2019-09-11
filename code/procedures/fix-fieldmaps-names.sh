6268825f149a346da34b859ea6b3cdc4dcdffc09
Kadelka, Tobias <t.kadelka@fz-juelich.de>
2019-09-11 14:03:45 +0200

    [DATALAD] added content

--- code/procedures/fix-fieldmaps-names.sh
+++ code/procedures/fix-fieldmaps-names.sh
@@ -2,26 +2,19 @@
 dataset=${1}
 subject=${2}
 
-
 for old_json in sub-DTA*/*/fmap/*fieldmap*.json ; do
 	if [ ! -z $(cat ${old_json} | jq .ImageType  | grep -v "ND" | grep -v "ORIGINAL"| grep -v "PRIMARY" | grep "P" ) ]; then
+
 		new_json="${old_json/fieldmap/phase}"
 		old_nifti="${old_json/.json/.nii.gz}"
 		new_nifti="${old_nifti/fieldmap/phase}"
 
-		echo $old_json
-		echo $new_json
-
 		mv $old_json $new_json
 		mv $old_nifti $new_nifti
-
 	else
 		new_json="${old_json/fieldmap/magnitude}"
                 old_nifti="${old_json/.json/.nii.gz}"
                 new_nifti="${old_nifti/fieldmap/magnitude}"
-
-                echo $old_json
-                echo $new_json
 
                 mv $old_json $new_json
                 mv $old_nifti $new_nifti
