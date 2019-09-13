0fc87489daac3f14286bf016490f645586f89f1b
Kadelka, Tobias <t.kadelka@fz-juelich.de>
2019-09-13 07:43:10 +0200

    test-script for killing the run-x part, that has no meaning in DTA and is WRONG.

---
+++ code/procedures/fix-useless-run-values.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+dataset=${1}
+subject=${2}
+
+for old_file in sub-DTA*/*/*/*run* ; do
+
+		tmp_file="${old_file/_run-1_/_}"
+		new_file="${tmp_file/_run-2_/_}"
+		#git mv $old_file $new_file
+		echo $old_file $new_file
+done
