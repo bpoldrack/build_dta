5dc9530025bfdd6979545721e21c4156eb4a15f9
Kadelka, Tobias <t.kadelka@fz-juelich.de>
2019-10-16 08:04:08 +0200

    update

--- code/create/create-dta.sh
+++ code/create/create-dta.sh
@@ -13,18 +13,18 @@ datalad install -d . -s git@github.com:bpoldrack/build_dta.git code/build_dta
 
 # modify the rules for the spec, so the dta-rules will be used
 git config -f .datalad/config --add datalad.hirni.dicom2spec.rules code/build_dta/code/create/dta_dicom2spec_rules.py
-datalad save
+datalad save -m "changed the hirni rules specification file"
 
 # hirni-import-dcm (but with reduced number of input tars)
 datalad install -d . -s git@github.com:TobiasKadelka/DTA_data.git sourcedata --recursive
-datalad save
+datalad save -m "installed the sourcedata that has to be converted into a bids-dataset"
 ./code/build_dta/code/routines/hirni-import-dcm.sh sourcedata
 
 # add procedures for correcting names to the studyspec.json
 for d in ./T* ; do
 	code/build_dta/code/routines/add-mods-to-specs.py $d/studyspec.json
 done
-datalad save
+datalad save -m "added modifications to studyspec.json files"
 
 # TODO: test the 3 lines from benjamins demo for really installing this as a dataset.
 
